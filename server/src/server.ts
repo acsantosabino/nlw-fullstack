3dae8171644aa54471aed3c86ccbbefdd80d1c84
Arthur Santos <acsantosabino@gmail.com>
2020-06-02 19:10:33 -0300

    Dia 1 - 1 Iniciando serviço API backend

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/users", (request, response) => {
+    console.log("Listagem de usuários");
+
+    // response.send("Hello World");
+    response.json(["Diego", "Cleiton", "Robson", "Daniel"]);
+});
+
+app.listen(3333);
